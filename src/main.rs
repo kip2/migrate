f4bc3532de9ac976cfdbf94eb4ef3f1c53543c6b
kip2 <tqa1no4@gmail.com>
2024-03-30 21:58:42 +0900

    Test_code: Add functionality to check for file existence and delete it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::fs::File;
+use std::io::{self, Write};
+use std::path::Path;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn create_file() -> bool {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use super::*;
+
+    #[test]
+    fn test_create_file() {
+        let filename = "./test/test_file.txt";
+
+        // cleansing test file.
+        if Path::new(filename).exists() {
+            let _ = fs::remove_file(filename).expect("File not Exists");
+        }
+
+        assert_eq!(true, true);
+
+        // cleansing test file.
+        if Path::new(filename).exists() {
+            let _ = fs::remove_file(filename).expect("File not Exists");
+        }
+    }
 }
