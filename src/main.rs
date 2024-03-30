6b620048a0147c2be9bb6aea978b098622272dc7
kip2 <tqa1no4@gmail.com>
2024-03-30 22:20:27 +0900

    test_code: create_file

--- src/main.rs
+++ src/main.rs
@@ -6,30 +6,55 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn create_file() -> bool {
-    unimplemented!()
+fn create_file(filepath: &str, contents: &str) -> io::Result<()> {
+    let mut file = File::create(filepath)?;
+    file.write_all(contents.as_bytes())?;
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    use std::fs;
-
     use super::*;
+    use std::fs;
+    use std::io::Read;
+
+    #[test]
+    fn test_read_file() {
+        assert_eq!(true, true);
+    }
 
     #[test]
     fn test_create_file() {
-        let filename = "./test/test_file.txt";
+        let filepath = "./test/test_file.txt";
 
         // cleansing test file.
-        if Path::new(filename).exists() {
-            let _ = fs::remove_file(filename).expect("File not Exists");
+        if Path::new(filepath).exists() {
+            let _ = fs::remove_file(filepath).expect("File not Exists");
         }
 
-        assert_eq!(true, true);
+        // test result to create file
+        assert!(create_file(filepath, "").is_ok());
+
+        // test create empty file.
+        let metadata = fs::metadata(filepath).unwrap();
+        assert_eq!(metadata.len(), 0);
 
         // cleansing test file.
-        if Path::new(filename).exists() {
-            let _ = fs::remove_file(filename).expect("File not Exists");
+        if Path::new(filepath).exists() {
+            let _ = fs::remove_file(filepath).expect("File not Exists");
+        }
+
+        // test create
+        // test result to create file
+        assert!(create_file(filepath, "Create Test!").is_ok());
+
+        // test create empty file.
+        let metadata = fs::metadata(filepath).unwrap();
+        assert_ne!(metadata.len(), 0);
+
+        // cleansing test file.
+        if Path::new(filepath).exists() {
+            let _ = fs::remove_file(filepath).expect("File not Exists");
         }
     }
 }
