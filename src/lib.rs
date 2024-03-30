349df26ed68edf4f5765f1b73dccdc38354f911d
kip2 <tqa1no4@gmail.com>
2024-03-30 23:14:09 +0900

    Delete test code and minor fixes

--- src/lib.rs
+++ src/lib.rs
@@ -2,27 +2,14 @@ mod file;
 
 use chrono::{Local, Utc};
 
-fn current_unix_time_stamp() -> i64 {
+fn unix_time_stamp() -> i64 {
     let now = Utc::now();
     let unix_time_stamp = now.timestamp();
     unix_time_stamp
 }
 
-fn current_jp_date() -> String {
+fn jp_date() -> String {
     let current_date = Local::now();
     let formatted_date = current_date.format("%Y-%m-%d").to_string();
     formatted_date
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_current_japanese_date() {
-        // data = "2024-03-30";
-        let current_date = Local::now();
-        let date = current_date.format("%Y-%m-%d").to_string();
-        assert_eq!(current_jp_date(), date);
-    }
 }
