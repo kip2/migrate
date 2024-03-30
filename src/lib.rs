227a6360e47ef3273f33841e3f664d7fb2ba3e1d
kip2 <tqa1no4@gmail.com>
2024-03-30 23:21:37 +0900

    Change visibility of modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,2 @@
-mod file;
-
-use chrono::{Local, Utc};
-
-fn unix_time_stamp() -> i64 {
-    let now = Utc::now();
-    let unix_time_stamp = now.timestamp();
-    unix_time_stamp
-}
-
-fn jp_date() -> String {
-    let current_date = Local::now();
-    let formatted_date = current_date.format("%Y-%m-%d").to_string();
-    formatted_date
-}
+pub mod file;
+pub mod time_util;
