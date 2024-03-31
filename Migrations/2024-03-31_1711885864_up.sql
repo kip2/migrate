36a667c70e27a3873807269f0e2404087a1692dd
kip2 <tqa1no4@gmail.com>
2024-03-31 20:52:52 +0900

    Add migration file for creating users table and posts table

---
+++ Migrations/2024-03-31_1711885864_up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE posts (
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                title VARCHAR(255) NOT NULL,
+                content TEXT NOT NULL,
+                created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                user_id BIGINT,
+                FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
